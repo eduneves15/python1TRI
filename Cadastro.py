3e812f8e1fde64c41b98be3e6608617c5885a519
eduneves15 <132684609+eduneves15@users.noreply.github.com>
2024-03-21 08:05:37 -0300

    Cadastro.py

--- Cadastro.py
+++ Cadastro.py
@@ -1,6 +1,6 @@
 def cadastrar_dados():
     nome = input("Digite o nome: ")
-    endereço = input("Digite o endereço: ")
+    endereço = input("Digite o endereço: "),
     with open("dados.txt", "a") as arquivo:
         arquivo.write(f"{nome};{endereço}\n")
     print("Dados cadastrados com sucesso!")
@@ -11,7 +11,7 @@ def ler_dados():
         with open("dados.txt", "r") as arquivo:
             print("Lista de dados cadastrados :")
             for linha in arquivo:
-                dados = linha.strip() .split(';')
+                dados = linha.strip().split(';')
                 if len(dados) == 2:
                     nome, endereco = dados
                     print(f"Nome: {nome}, Endereço: {endereco}")
@@ -26,10 +26,11 @@ def main():
         print("\n===== Menu de Opções =====")
         print("1. Cadastrar dados")
         print("2. Ler dados cadastrados")
+        print("3. Sair")
 
         opcao = input("Escolha uma opção:")
 
-        if opcao == " ":
+        if opcao == "1":
             cadastrar_dados()
         elif opcao == "2":
             ler_dados()
